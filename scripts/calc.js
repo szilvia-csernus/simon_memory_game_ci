770a09dcb393d425c02c912738f0637f31dc4e46
Szilvia Csernus <csernus.szilvi@gmail.com>
2023-02-04 11:54:20 +0000

    initial commit

---
+++ scripts/calc.js
@@ -0,0 +1,3 @@
+const addition = (num1, num2) => num1 + num2;
+
+module.exports = addition;
---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const addition = require('../calc.js');
+
+describe('Calculator', () => {
+    describe('Addition function', () => {
+        test('should return 42 for 20 + 22', () => {
+            expect(addition(20,22)).toBe(42);
+        });
+        test('should return 15 for 10 + 5', () => {
+            expect(addition(10,5)).toBe(15);
+        });
+        test('should return 'error' for 20 + 22', () => {
+            expect(addition(20,22)).toBe(42);
+        });
+    })
+    // describe('Subtraction function', () => {
+    //     test('should return 18 for 20 - 2', () => {
+    //         expect(addition(20,2)).toBe(18);
+    //     })
+    // })
+    // describe('Multiplication function', () => {
+    //     test('should return 44 for 2 * 22', () => {
+    //         expect(addition(2,22)).toBe(44);
+    //     })
+    // })
+    // describe('Division function', () => {
+    //     test('should return 2 for 22 / 11', () => {
+    //         expect(addition(22,11)).toBe(2);
+    //     })
+    // })
+})
